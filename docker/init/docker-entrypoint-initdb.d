5d7d84585416c05ef355584ce0dd80a098efac8e
Gabriel Crevelario <terceiro-gcoliveira@icatuseguros.com.br>
2024-02-23 02:39:14 -0300

    feature: add configs  for project

--- docker/init/docker-entrypoint-initdb.d
+++ docker/init/docker-entrypoint-initdb.d
@@ -1,1 +1,34 @@
-GRANT ALL PRIVILEGES ON DATABASE mydatabase TO myuser;
+use desafioVotacao
+db.getCollection("pauta").insertOne({
+    "_id" : ObjectId("6110e0129df3a5a55f6df739"),
+    "title" : "Título da Pauta",
+    "description" : "Descrição da Pauta",
+    "dateStart" : ISODate("2024-02-21T15:30:00.000+0000"),
+    "dateEnd" : ISODate("2024-02-21T15:31:00.000+0000"),
+    "votos" : [
+        {
+            "_id" : ObjectId("6110e0129df3a5a55f6df73a"),
+            "username" : "Usuário 1",
+            "voto" : true,
+            "dateStart" : ISODate("2024-02-21T15:30:00.000+0000"),
+            "pautaId" : ObjectId("6110e0129df3a5a55f6df739"),
+            "usuario" : {
+                "_id" : ObjectId("6110e0129df3a5a55f6df73a"),
+                "name" : "glauber",
+                "cpf" : "15631960770",
+                "email" : "gabriel@gmail.com",
+                "password" : "123456789"
+            }
+        }
+    ],
+    "ativated" : true
+})
+db.getCollection("usuario").insertOne({
+   
+    
+     "name":"teste",
+    "cpf":"945.096.810-11",
+    "email":"teste@gmail.com",
+    "password":"123456789"
+})
+
--- src/main/java/com/desafiovotacao/desafiovotacao/config/GlobalExceptionHandler.java
+++ src/main/java/com/desafiovotacao/desafiovotacao/config/GlobalExceptionHandler.java
@@ -1,2 +1,26 @@
-package com.desafiovotacao.desafiovotacao.config;public class GlobalExceptionHandler {
+package com.desafiovotacao.desafiovotacao.config;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Object> handleException(Exception ex) {
+        // Aqui você pode extrair a mensagem da exceção e retorná-la como parte da resposta da API
+        String mensagemDeErro = ex.getMessage();
+
+        // Crie um objeto para encapsular a mensagem de erro (opcional)
+        Map<String, String> responseBody = new HashMap<>();
+        responseBody.put("mensagem", mensagemDeErro);
+
+        // Retorna uma resposta com o status HTTP apropriado e a mensagem de erro
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(responseBody);
+    }
 }
