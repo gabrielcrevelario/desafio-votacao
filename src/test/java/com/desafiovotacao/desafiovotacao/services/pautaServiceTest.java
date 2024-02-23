b4eaa516d0f33ccd32a684749578141730eb49dc
Gabriel Crevelario <terceiro-gcoliveira@icatuseguros.com.br>
2024-02-23 02:39:54 -0300

    feature: add class unit tests  for project

--- src/test/java/com/desafiovotacao/desafiovotacao/services/pautaServiceTest.java
+++ src/test/java/com/desafiovotacao/desafiovotacao/services/pautaServiceTest.java
@@ -1,2 +1,112 @@
-package com.desafiovotacao.desafiovotacao.services;public class pautaServiceTest {
+package com.desafiovotacao.desafiovotacao.services;
+
+import com.desafiovotacao.desafiovotacao.entities.Pauta;
+import com.desafiovotacao.desafiovotacao.entities.Usuario;
+import com.desafiovotacao.desafiovotacao.entities.Voto;
+import com.desafiovotacao.desafiovotacao.repositories.PautaRepository;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.mongodb.core.ReactiveMongoTemplate;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+
+import java.time.LocalDateTime;
+import java.time.ZonedDateTime;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest
+public class pautaServiceTest {
+    @InjectMocks
+    private PautaService pautaService;
+    PautaRepository pautaRepository = mock(PautaRepository.class);
+    @Mock
+    private ReactiveMongoTemplate reactiveMongoTemplate;
+
+
+    @Test
+    public void testCreate() {
+        // Crie uma pauta de exemplo
+        Pauta pauta = new Pauta();
+        pauta.setId("fd31bca1ef884b4aa43d7cb053ccbf3a");
+        pauta.setTitle("Pauta 1");
+        pauta.setDescription("Descrição da pauta 1");
+        pauta.setDateStart(LocalDateTime.now());
+        pauta.setDateEnd(LocalDateTime.now().plusMinutes(1)); // Pauta ativa por 1 minuto
+
+        // Mock o comportamento do ReactiveMongoTemplate
+        when(pautaRepository.insert(pauta)).thenReturn(Mono.just(pauta));
+        // Chame o método de criação da pauta
+        Mono<Pauta> pautaMono = pautaService.create(pauta);
+
+        // Verifique se a pauta foi criada corretamente
+        StepVerifier.create(pautaMono)
+                .expectNext(pauta) // Verifique se a pauta retornada é igual à pauta de exemplo
+                .verifyComplete();
+    }
+    @Test
+    public void testCreateUpdate() {
+        var id = "fd31bca1ef884b4aa43d7cb053ccbf3a";
+        Pauta pauta = new Pauta();
+        pauta.setId(id);
+        pauta.setTitle("Pauta 1");
+        pauta.setDescription("Descrição da pauta 1");
+        pauta.setDateStart(LocalDateTime.now());
+        pauta.setDateEnd(LocalDateTime.now().plusMinutes(1)); // Pauta ativa por 1 minuto
+
+        when(pautaRepository.existsById(id)).thenReturn(Mono.just(true));
+        when(pautaRepository.save(pauta)).thenReturn(Mono.just(pauta));
+        // Chame o método de criação da pauta
+        Mono<Pauta> pautaMono = pautaService.update(id, pauta);
+
+        // Verifique se a pauta foi criada corretamente
+        StepVerifier.create(pautaMono)
+                .expectNext(pauta) // Verifique se a pauta retornada é igual à pauta de exemplo
+                .verifyComplete();
+    }
+    @Test
+    public void testCreateUpdateIfIdNotExists() throws Exception {
+        var id = "fd31bca1ef884b4aa43d7cb053ccbf3a";
+        Pauta pauta = new Pauta();
+        pauta.setId(id);
+        pauta.setTitle("Pauta 1");
+        pauta.setDescription("Descrição da pauta 1");
+        pauta.setDateStart(LocalDateTime.now());
+        pauta.setDateEnd(LocalDateTime.now().plusMinutes(1)); // Pauta ativa por 1 minuto
+
+        when(pautaRepository.existsById(id)).thenReturn(Mono.just(false));
+        when(pautaRepository.save(pauta)).thenReturn(Mono.just(pauta));
+        // Chame o método de criação da pauta
+        Mono<Pauta> pautaMono = pautaService.update(id, pauta);
+
+        // Verifique se a pauta foi criada corretamente
+        StepVerifier.create(pautaMono)
+                .expectErrorMatches(throwable -> throwable.getMessage().equals("pauta não existe"))
+                .verify();
+    }
+    @Test
+    public void testVerifyPautasIsActive() {
+        LocalDateTime dataInicio = LocalDateTime.now().minusMinutes(2); // Pauta ativa há mais de 1 minuto
+        Pauta pauta = new Pauta();
+        pauta.setDateStart(dataInicio);
+        pauta.setAtivated(true);
+
+        when(reactiveMongoTemplate.find(Mockito.any(), Mockito.eq(Pauta.class)))
+                .thenReturn(Flux.just(pauta)); // Simula a busca de uma pauta ativa há mais de 1 minuto
+
+        when(reactiveMongoTemplate.save(Mockito.any(Pauta.class)))
+                .thenReturn(Mono.just(pauta)); // Simula a atualização da pauta
+
+        StepVerifier.create(pautaService.verifyPautasIsActive())
+                .expectNextMatches(updatedPauta -> !updatedPauta.isAtivated()) // Verifica se a pauta foi desativada
+                .expectComplete()
+                .verify();
+    }
 }
--- src/test/java/com/desafiovotacao/desafiovotacao/services/votoServiceTest.java
+++ src/test/java/com/desafiovotacao/desafiovotacao/services/votoServiceTest.java
@@ -1,4 +1,169 @@
 package com.desafiovotacao.desafiovotacao.services;
 
-public class pautaServiceTest {
+import com.desafiovotacao.desafiovotacao.entities.Pauta;
+import com.desafiovotacao.desafiovotacao.entities.Usuario;
+import com.desafiovotacao.desafiovotacao.entities.Voto;
+import com.desafiovotacao.desafiovotacao.repositories.PautaRepository;
+import com.desafiovotacao.desafiovotacao.repositories.UsuarioRepository;
+import com.desafiovotacao.desafiovotacao.repositories.VotoRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.mongodb.core.ReactiveMongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import java.time.LocalDateTime;
+import java.util.Arrays;
+import org.springframework.data.mongodb.core.query.Query;
+import static org.mockito.Mockito.eq;
+@SpringBootTest
+    public class votoServiceTest {
+    @Mock
+    private PautaService pautaService;
+    @Mock
+    PautaRepository pautaRepository;
+    @Mock
+    private ReactiveMongoTemplate reactiveMongoTemplate;
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+    }
+    @Test
+    public void testFindAllByPautaId() {
+        var usuarioService = new UsuarioService(usuarioRepository,reactiveMongoTemplate);
+        var pautaServiceInstance = new PautaService(pautaRepository, reactiveMongoTemplate);
+        var votoServiceInstance = new VotoService(pautaServiceInstance, usuarioService);
+
+
+        Pauta pauta = new Pauta();
+        pauta.setId("pautaId");
+        pauta.setVotos(Arrays.asList(
+                new Voto("votoId1", true, LocalDateTime.now(), "pautaId","userId", new Usuario()),
+                new Voto("votoId2",  false, LocalDateTime.now(), "pautaId", "userId",new Usuario())
+        ));
+
+        // Configure o comportamento do pautaService.findPautabyId para retornar a pauta criada
+        when(pautaRepository.findById("pautaId")).thenReturn(Mono.just(pauta));
+
+        // Chame o método que você está testando
+        Flux<Voto> result = votoServiceInstance.findAllByPautaId("pautaId");
+
+        // Verifique se o fluxo de votos foi emitido corretamente
+        StepVerifier.create(result)
+                .expectNextMatches(voto -> voto.getId().equals("votoId1"))
+                .expectNextMatches(voto -> voto.getId().equals("votoId2"))
+                .verifyComplete();
+    }
+    @Test
+    public void testCreateVoteSuccess() throws Exception {
+        var usuarioService = new UsuarioService(usuarioRepository,reactiveMongoTemplate);
+        // Configurar pautaService.findPautabyId para retornar uma pauta válida
+        var pautaServiceInstance = new PautaService(pautaRepository, reactiveMongoTemplate);
+        var votoServiceInstance = new VotoService(pautaServiceInstance, usuarioService);
+        Pauta pauta = new Pauta();
+        Voto voto = new Voto();
+        Usuario usuario = new Usuario();
+        usuario.setCpf("cpf3");
+
+        voto.setUsuario(usuario);
+        pauta.setId("pautaId");
+        pauta.setTitle("Pauta de Teste");
+        pauta.setDateStart(LocalDateTime.now());
+        pauta.setDateEnd(LocalDateTime.now().plusMinutes(1));
+
+        Usuario newUsuario = new Usuario();
+        newUsuario.setCpf("cpf3");
+
+        pauta.setVotos(Arrays.asList(voto)); // Pauta sem votos
+
+        when(pautaRepository.findById("pautaId")).thenReturn(Mono.just(pauta));
+        when(pautaRepository.existsById("pautaId")).thenReturn(Mono.just(true));
+        when(pautaRepository.save(pauta)).thenReturn(Mono.just(pauta));
+        when(reactiveMongoTemplate.findOne(
+                Query.query(Criteria.where("cpf").is("cpf2")),Usuario.class
+        )).thenReturn(Mono.just(usuario));
+        Mono<Pauta> result = votoServiceInstance.create("pautaId", "cpf2", "sim");
+
+        StepVerifier.create(result)
+                .expectNext(pauta)
+                .verifyComplete();
+    }
+    @Test
+    public void testCreateVotoFaillWithMinutes() throws Exception {
+
+        // Configurar pautaService.findPautabyId para retornar uma pauta válida
+        var usuarioService = new UsuarioService(usuarioRepository,reactiveMongoTemplate);
+        var pautaServiceInstance = new PautaService(pautaRepository, reactiveMongoTemplate);
+        var votoServiceInstance = new VotoService(pautaServiceInstance,usuarioService );
+        Pauta pauta = new Pauta();
+        Voto voto = new Voto();
+        Usuario usuario = new Usuario();
+        usuario.setCpf("cpf2");
+        voto.setUsuario(usuario);
+        pauta.setId("pautaId");
+        pauta.setTitle("Pauta de Teste");
+        pauta.setDateStart(LocalDateTime.now().minusMinutes(1));
+        pauta.setDateEnd(LocalDateTime.now().plusMinutes(1));
+
+        pauta.setVotos(Arrays.asList(voto)); // Pauta sem votos
+
+        when(pautaRepository.findById("pautaId")).thenReturn(Mono.just(pauta));
+        when(pautaRepository.existsById("pautaId")).thenReturn(Mono.just(true));
+        when(pautaRepository.save(pauta)).thenReturn(Mono.just(pauta));
+        Mono<Pauta> result = votoServiceInstance.create("pautaId", "cpf", "sim");
+        // Configurar pautaService.update prara retornar a pauta atualizada
+
+        // Chamar o método que você está testando
+
+
+        // Verificar se a criação do voto foi bem-sucedida
+        StepVerifier.create(result)
+                .expectErrorMatches(throwable -> throwable.getMessage().equals("O Horario de votação finalizou para a pauta "+ pauta.getTitle()))
+                .verify(); // Verifica se as expectativas foram atendidas
+    }
+    @Test
+    public void testCreateVotoFaillUserExists() throws Exception {
+
+        var usuarioService = new UsuarioService(usuarioRepository,reactiveMongoTemplate);
+        var pautaServiceInstance = new PautaService(pautaRepository, reactiveMongoTemplate);
+        var votoServiceInstance = new VotoService(pautaServiceInstance, usuarioService);
+        Pauta pauta = new Pauta();
+        Voto voto = new Voto();
+        Usuario usuario = new Usuario();
+        usuario.setCpf("cpf");
+
+        // Configurar o comportamento esperado para o mock do Usuario
+//        when(usuario.getCpf()).thenReturn("cpf");
+
+        voto.setUsuario(usuario);
+        pauta.setId("pautaId");
+        pauta.setTitle("Pauta de Teste");
+        pauta.setDateStart(LocalDateTime.now());
+        pauta.setDateEnd(LocalDateTime.now().plusMinutes(1));
+
+        pauta.setVotos(Arrays.asList(voto)); // Pauta sem votos
+
+        when(pautaRepository.findById("pautaId")).thenReturn(Mono.just(pauta));
+        when(pautaRepository.existsById("pautaId")).thenReturn(Mono.just(true));
+        when(pautaRepository.save(pauta)).thenReturn(Mono.just(pauta));
+        Mono<Pauta> result = votoServiceInstance.create("pautaId", "cpf", "sim");
+        // Configurar pautaService.update prara retornar a pauta atualizada
+
+        // Chamar o método que você está testando
+
+
+        // Verificar se a criação do voto foi bem-sucedida
+        StepVerifier.create(result)
+                .expectErrorMatches(throwable -> throwable.getMessage().equals("Usuario já votou"))
+                .verify(); // Verifica se as expectativas foram atendidas
+    }
 }
